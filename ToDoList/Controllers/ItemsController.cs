fc4a14b7d657e224db32352b14efe119afcedd99
Mark W <markwood117@gmail.com>
2018-01-02 14:07:22 -0800

    initial commit; basic todolist using .NET

---
+++ ToDoList/Controllers/ItemsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using ToDoList.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ToDoList.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private ToDoListContext db = new ToDoListContext();
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            List<Item> model = db.Items.ToList();
+            return View(model);
+        }
+    }
+}
