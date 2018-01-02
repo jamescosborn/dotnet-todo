7a2a884543a745b5e69b7de26659bdcb4d69c6e1
Mark W <markwood117@gmail.com>
2018-01-02 15:29:01 -0800

    add delete&update code(still in progress)

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 using ToDoList.Models;
 
@@ -30,11 +31,41 @@ namespace ToDoList.Controllers
 		{
 			return View();
 		}
-
 		[HttpPost]
 		public IActionResult Create(Item item)
 		{
 			db.Items.Add(item);
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		public IActionResult Edit(int id)
+		{
+			var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
+			return View(thisItem);
+		}
+		[HttpPost]
+		public IActionResult Edit(Item item)
+		{
+			db.Entry(item).State = EntityState.Modified;
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		public IActionResult Delete(int id)
+		{
+			var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
+			return View(thisItem);
+		}
+        /*The attribute ActionName, seen here, lets us give the Action a different
+        name than used in the method signature.So, even though our method is named
+        DeleteConfirmed, because we set the attribute ActionName equal to Delete,
+        the proper action will execute when the form is submitted.*/
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeleteConfirmed(int id)
+		{
+			var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
+			db.Items.Remove(thisItem);
 			db.SaveChanges();
 			return RedirectToAction("Index");
 		}
